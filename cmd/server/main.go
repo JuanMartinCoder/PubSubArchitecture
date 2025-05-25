644d4d303a10892bcabc520e5aeb407061589b18
JuanMartinCoder <skyzinformatica@gmail.com>
2025-05-24 23:59:14 -0300

    Second commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connstr := "amqp://guest:guest@localhost:5672/"
+
+	Conn, err := amqp.Dial(connstr)
+	if err != nil {
+		log.Fatalf("Error connecting %v", err)
+	}
+	defer Conn.Close()
+	fmt.Println("Connection successfully:")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
